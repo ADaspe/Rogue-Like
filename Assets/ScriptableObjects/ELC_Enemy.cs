cf8af43e0d9df284a85cf2a2a8db065133c58f10
Eliott <eliott.chauchard@orange.fr>
2020-09-28 16:46:39 +0200

    Trying to do something

--- Assets/ScriptableObjects/ELC_Enemy.cs
+++ Assets/ScriptableObjects/ELC_Enemy.cs
@@ -8,13 +8,24 @@ public class ELC_Enemy : ScriptableObject
     public float maxHeal;
     public float strenght;
     public float speed;
+    public float moneyWhenHit;
+    public float moneyWhenDead;
+
     public bool followPlayer;
     public bool fleePlayer;
     public bool stayAtDistance;
     public float distance;
+
     public bool fireBullets;
-    public bool instantiateEnemyAtDeath;
+    public GameObject bullet;
+
+    public bool instantiateSomethingAtDeath;
+    public GameObject objectToInstantiate;
+
     public bool dash;
+    public float dashSpeed;
+    public float dashTime;
+
     public Sprite sprite;
     public MonoBehaviour AIScript;
     public Animator Animator;
