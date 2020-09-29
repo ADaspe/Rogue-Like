8efe821b80ccb4efd14790a4c1b3c29577caf7cd
Geoff <geoffroy.carrot@rubika-edu.com>
2020-09-29 12:39:36 +0200

    Barre de vie
    Ajout de la barre de vie, elle prend des dégâts quand on appuie sur K et elle descend avec le temps

---
+++ Assets/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider slider;
+    public float lifeLost;
+
+    public void SetMaxHealth(int health)
+    {
+       slider.maxValue = health;
+       slider.value = health;
+    }
+
+    public void SetHealth(float health)
+    {
+        slider.value = health;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        slider.value -= Time.deltaTime * lifeLost;
+    }
+}
---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public float currentHealth;
+
+    public Health healthBar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Damage(20);
+        }
+    }
+    void Damage(int damage)
+    {
+        currentHealth = healthBar.slider.value;
+        currentHealth -= damage;
+
+        healthBar.SetHealth(currentHealth);
+    }
+}
