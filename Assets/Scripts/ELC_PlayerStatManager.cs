940dfced34a41f98e1e90d4eec6342af2a3ff9e2
Eliott <eliott.chauchard@gmail.com>
2020-09-30 21:18:08 +0200

    Début PlayerStatManager

---
+++ Assets/Scripts/ELC_PlayerStatManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELC_PlayerStatManager : MonoBehaviour
+{
+    public float Speed;
+    public float MaxHealth;
+    public float Resistance;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
