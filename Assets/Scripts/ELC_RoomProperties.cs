c84498c71403611f17f47f56a23d7d1d20238aec
Eliott <eliott.chauchard@orange.fr>
2020-10-20 17:00:45 +0200

    Début génération procédurale

---
+++ Assets/Scripts/ELC_RoomProperties.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class ELC_RoomProperties : MonoBehaviour
+{
+    public bool openLeftDoor;
+    public bool openRightDoor;
+    public bool openTopDoor;
+    public bool openDownDoor;
+
+    public Vector2 positionNumber;
+
+}
---
+++ Assets/Scripts/ELC_RoomsGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ELC_RoomsGenerator : MonoBehaviour
+{
+    public int arrayDimentionX;
+    public int arrayDimentionY;
+
+
+    public int distanceBtwRoomsX;
+    public int distanceBtwRoomsY;
+
+    public Vector2 entryPosition;
+
+    public GameObject roomChecker;
+
+    public List<GameObject> roomsList;
+
+    public GameObject[,] checkersArray;
+    public GameObject[,] roomsArray;
+
+
+    
+    void Start()
+    {
+        checkersArray = new GameObject[arrayDimentionX, arrayDimentionY];
+        roomsArray = new GameObject[arrayDimentionX, arrayDimentionY];
+
+        for (int i = 0; i < arrayDimentionX; i++)
+        {
+            for (int e = 0; e < arrayDimentionY; e++)
+            {
+                GameObject go = GameObject.Instantiate(roomChecker, new Vector3(i * distanceBtwRoomsX, -e * distanceBtwRoomsY), Quaternion.identity);
+                checkersArray[i, e] = go;
+            }
+        }
+        SpawnRandomRoom(checkersArray[(int)entryPosition.x, (int)entryPosition.y]);
+    }
+
+
+
+    private void SpawnRandomRoom(GameObject checkerObject)
+    {
+        int randomNumber = Random.Range(0, roomsList.Count);
+        GameObject.Instantiate(roomsList[randomNumber], checkerObject.transform);
+    }
+}
