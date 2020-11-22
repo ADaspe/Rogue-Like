93204beb4b1f163386181f07707a5defc2fb7801
Emeric Delemer <emericdelemer@gmail.com>
2020-11-22 15:51:24 +0100

    Dialogue1

---
+++ Assets/Scripts/EMD_Scripts/EMD_NPCIsTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EMD_NPCIsTrigger : MonoBehaviour
+{
+    public bool IsInRange = false;
+    public bool DialogueOn = false;
+
+
+    private void Update()
+    {
+        //Si le player est près du npc et qu'il appuie sur "e" le dialogue démarre 
+        if (Input.GetAxisRaw("Interact") == 1 && IsInRange == true)
+        {
+            DialogueOn = true; 
+        }
+    }
+
+    //Detecter si le player est près du NPC
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        IsInRange = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        IsInRange = false;
+    }
+    
+}
+
