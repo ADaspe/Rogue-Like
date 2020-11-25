013b1b60df7ef7fd8d3594dedebc86bd07c43c94
Alexandre Daspe <alexandre.daspe@gmail.com>
2020-11-25 21:46:33 +0100

    Achievement system : On

---
+++ Assets/ScriptableObjects/AXD_AchievementSO.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Objects", menuName = "ScriptableObjects/AchivementScriptableObject", order = 1)]
+public class AXD_AchievementSO : ScriptableObject
+{
+    public string name;
+    public int achievementTier;
+    public ELC_Enemy ennemyToDefeat;
+    public int numberToDefeat;
+    public int numberDefeated;
+    public bool isUnlocked;
+    //public ELC_Passif passifToUnlock;
+
+    public void AddDefeated(int defeated = 1)
+    {
+        if (!isUnlocked)
+        {
+            //Debug.Log("Achivement " + name + achievementTier + " progress");
+            numberDefeated += defeated;
+            if(numberDefeated >= numberToDefeat)
+            {
+                isUnlocked = true;
+                //Debug.Log("Achivement " + name + achievementTier + " unlocked !");
+            }
+        }
+    }
+
+    public void ResetAchievement()
+    {
+        isUnlocked = false;
+        numberDefeated = 0;
+    }
+}
