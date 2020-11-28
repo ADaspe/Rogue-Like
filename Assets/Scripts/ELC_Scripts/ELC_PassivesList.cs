92bd80466294f216d4e28952dba85db79592a7ba
Eliott <eliott.chauchard@gmail.com>
2020-11-28 16:47:01 +0100

    Système de passifs terminé + 1 passif de fonctionnel + ramassage

---
+++ Assets/Scripts/ELC_Scripts/ELC_PassivesList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELC_PassivesList : MonoBehaviour
+{
+    
+    public List<GameObject> PassivesList;
+}
--- Assets/Scripts/ELC_Scripts/ELC_PickupItems.cs
+++ Assets/Scripts/ELC_Scripts/ELC_PickupItems.cs
@@ -6,9 +6,16 @@ public class ELC_PickupItems : MonoBehaviour
 {
     private ELC_ObjectsInventory ObjectsInv;
     private ELC_Detector detector;
+
+
     public GameObject Object;
-    public enum CollectibleTypes {Object, PowerUp};
+    public ELC_PassiveSO Passive;
+
+
+    public enum CollectibleTypes {Object, PowerUp, Passive};
     public CollectibleTypes Type = CollectibleTypes.Object;
+
+
     private void Start()
     {
         ObjectsInv = GameObject.Find("PlayerInventory").GetComponent<ELC_ObjectsInventory>();
@@ -20,6 +27,9 @@ public class ELC_PickupItems : MonoBehaviour
         if (detector.playerIsInside && this.gameObject.CompareTag("Collectible") && Input.GetButtonDown("Interact"))
         {
             if(Type == CollectibleTypes.Object) ObjectsInv.AddObject(Object, Object.GetComponent<ELC_ObjectsUse>().ObjectsScriptableObject.quantity);
+
+            if (Type == CollectibleTypes.Passive) ELC_ObjectsInventory.ActivePassif = Passive;
         }
+        
     }
 }
