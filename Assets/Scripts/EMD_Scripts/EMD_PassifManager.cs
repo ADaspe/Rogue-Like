f04eef3680e5fce4519f6bf0355130fad30977ee
Emeric Delemer <emericdelemer@gmail.com>
2021-01-13 23:27:00 +0100

    PNJ'sSansBugs

--- Assets/Scripts/EMD_Scripts/EMD_PassifManager.cs
+++ Assets/Scripts/EMD_Scripts/EMD_PassifManager.cs
@@ -19,6 +19,7 @@ public class EMD_PassifManager : MonoBehaviour
     public GameObject QuittButton;
     public TextMeshProUGUI PassivePrice;
     public TextMeshProUGUI textDisplay;
+    private EMD_DialogueManager DialogueManagerScript;
     //public GameObject passif1;
     public Image image1;
     public Image image2;
@@ -28,6 +29,7 @@ public class EMD_PassifManager : MonoBehaviour
     void Start()
     {
         RandomPick();
+        DialogueManagerScript = FindObjectOfType<EMD_DialogueManager>();
     }
 
     /*void takePassiveAndCreateArray()
@@ -96,18 +98,28 @@ public class EMD_PassifManager : MonoBehaviour
 
     public void Selected1()
     {
-        SelectedPassive = passive[0];
-        PassivePrice.text = "Prix : " + passive[0].PassivePrice.ToString();
+        Debug.Log(DialogueManagerScript.IsWriting);
+        if (DialogueManagerScript.IsWriting == false)
+        {
+            SelectedPassive = passive[0];
+            PassivePrice.text = "Prix : " + passive[0].PassivePrice.ToString();
+        }
     }
     public void Selected2()
     {
-        SelectedPassive = passive[1];
-        PassivePrice.text = "Prix : " + passive[1].PassivePrice.ToString();
+        if (DialogueManagerScript.IsWriting == false)
+        {
+            SelectedPassive = passive[1];
+            PassivePrice.text = "Prix : " + passive[1].PassivePrice.ToString();
+        }
     }
     public void Selected3()
     {
-        SelectedPassive = passive[2];
-        PassivePrice.text = "Prix : " + passive[2].PassivePrice.ToString();
+        if (DialogueManagerScript.IsWriting == false)
+        {
+            SelectedPassive = passive[2];
+            PassivePrice.text = "Prix : " + passive[2].PassivePrice.ToString();
+        }
     }
 
     public void ChoosenPassive()
