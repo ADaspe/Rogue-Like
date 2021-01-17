6cf2b412163642338f2bfa210654137679520808
Eliott <eliott.chauchard@gmail.com>
2021-01-17 23:05:24 +0100

    Revert "Revert "Glow Berserk""
    This reverts commit 36e9ad2cc3f496307de7fca781a0b578e30f55aa.

--- Assets/ELC_BerserkAura.cs
+++ Assets/ELC_BerserkAura.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class ELC_BerserkAura : MonoBehaviour
 {
-    
+    public float maxLightIntensity;
+    public float emissionMax;
+    public ParticleSystem particleSys;
+    public Light2D light2D;
+    public ELC_PlayerStatManager playerStats;
 
-    
     void Update()
     {
-        
+        var emission = particleSys.emission;
+        emission.rateOverTime = emissionMax * (1 - (playerStats.currentHealth / playerStats.MaxHealth));
+        light2D.intensity = maxLightIntensity * (1 - (playerStats.currentHealth / playerStats.MaxHealth));
     }
 }
