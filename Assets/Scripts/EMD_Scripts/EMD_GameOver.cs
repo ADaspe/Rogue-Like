adc82281b9d39c6f3ba99d5571b367e8c1c5d8fc
Emeric Delemer <emericdelemer@gmail.com>
2021-01-18 16:19:55 +0100

    GameOverMieux

--- Assets/Scripts/EMD_Scripts/EMD_GameOver.cs
+++ Assets/Scripts/EMD_Scripts/EMD_GameOver.cs
@@ -3,35 +3,74 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-using System.Threading;
 
 public class EMD_GameOver : MonoBehaviour
 {
+    public GameObject GameOverGO;
+    public GameObject MonsterWhoKilledGO;
+    public GameObject RunTimeGO;
+    public GameObject PlusGrosComboGO;
+    public GameObject ThunesGO;
+    public GameObject PassifUsedGO;
     public TextMeshProUGUI GameOver;
     public TextMeshProUGUI MonsterWhoKilled;
     public TextMeshProUGUI RunTime;
     public TextMeshProUGUI PlusGrosCombo;
     public TextMeshProUGUI Thunes;
     public TextMeshProUGUI PassifUsed;
+    int length = 6;
+    public float DelayTime;
 
     // Start is called before the first frame update
 
     public string HUB;
     public string MainMenu;
-
+  
     private void Start()
     {
-        //met le GameOver
-        Thread.Sleep(3000);
-        _MonsterWhoKilled();
-        Thread.Sleep(3000);
-        _RunTime();
-        Thread.Sleep(3000);
-        _PlusGrosCombo();
-        Thread.Sleep(3000);
-        _Thunes();
-        Thread.Sleep(3000);
-        _PassifUsed();
+        StartCoroutine("OneByOne");
+    }
+
+    IEnumerator OneByOne()
+    {
+        for (int i = 0; i < length; i++)
+        {
+            if (i == 0)
+            {
+                GameOverGO.SetActive(true);
+                yield return new WaitForSeconds(DelayTime);
+            }
+            if (i == 1)
+            {
+                //MonsterWhoKilled.text = "TUE PAR: " + PlayerHealth.lastHitEnnemy.Name;
+                MonsterWhoKilledGO.SetActive(true);
+                yield return new WaitForSeconds(DelayTime);
+            }
+            if (i == 2)
+            {
+                //RunTime.text = "TEMPS DE LA RUN:    " + Timer;
+                RunTimeGO.SetActive(true);
+                yield return new WaitForSeconds(DelayTime);
+            }
+            if (i == 3)
+            {
+                //PluGrosCombo.text = "LE PLUS GROS COMBO:    " + la variable du combo;
+                PlusGrosComboGO.SetActive(true);
+                yield return new WaitForSeconds(DelayTime);
+            }
+            if (i == 4)
+            {
+                //Thunes.text = "ARGENT RECOLTE:    " + L'argent des caisses;
+                ThunesGO.SetActive(true);
+                yield return new WaitForSeconds(DelayTime);
+            }
+            if (i == 5)
+            {
+                PassifUsed.text = "PASSIF UTILISE : " + ELC_ObjectsInventory.ActivePassif;
+                PassifUsedGO.SetActive(true);
+                yield return new WaitForSeconds(DelayTime);
+            }
+        }
     }
 
     public void GoHUB()
@@ -42,31 +81,5 @@ public class EMD_GameOver : MonoBehaviour
     public void GoMainMenu()
     {
         SceneManager.LoadScene(MainMenu);
-    }
-
-    public void _PassifUsed()
-    {
-        PassifUsed.text = "PASSIF UTILISE " + ELC_ObjectsInventory.ActivePassif;
-        //l'afficher
-    }
-    public void _MonsterWhoKilled()
-    {
-        //MonsterWhoKilled.text = "TUE PAR: " + DernierMonstre;
-        //l'afficher
-    }
-    public void _RunTime()
-    {
-        //RunTime.text = "TEMPS DE LA RUN:    " + la variable du timer;
-        //l'afficher
-    }
-    public void _PlusGrosCombo()
-    {
-        //PluGrosCombo.text = "LE PLUS GROS COMBO:    " + la variable du combo;
-        //l'afficher
-    }
-    public void _Thunes()
-    {
-        //Thunes.text = "ARGENT RECOLTE:    " + L'argent des caisses;
-        //l'afficher
     }
 }
