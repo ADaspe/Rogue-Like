d939d0900f439620b06f87a0d3b1952afed0b365
Lucrezia <lucrezia.romeo@gmail.com>
2021-01-19 21:32:32 +0100

    Menu Pause décoré

---
+++ Assets/RandomFlicker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomFlicker : MonoBehaviour
+{
+    //If you use Int in the Random.Range() then it will
+    //return an Int
+    public float RanTimerHigh = 14f;
+    public float RanTimerLow = 6f;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        float Randomfloat = Random.Range(RanTimerLow, RanTimerHigh);
+        rend.material.SetFloat("_speedValue", Randomfloat);
+    }
+}
