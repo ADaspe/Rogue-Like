eec78dd42fa5b7dd9abe6746563337da915caa12
Geoff <geoffroy.carrot@rubika-edu.com>
2021-01-20 22:46:22 +0100

    Encore du sound design, et le SLIDER dans le menu option

---
+++ Assets/GFC_Volume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GFC_Volume : MonoBehaviour
+{
+    public AudioListener listener;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void ChangeVolume (float newValue)
+    {
+        float newVolume = AudioListener.volume;
+        newVolume = newValue;
+        AudioListener.volume = newVolume;
+        PlayerPrefs.SetFloat("Volume", newVolume);
+    }
+}
