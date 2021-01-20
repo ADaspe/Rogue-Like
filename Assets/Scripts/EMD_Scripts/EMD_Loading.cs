88a1a7fa3412bc2d99731b3fc032360428230810
Emeric Delemer <emericdelemer@gmail.com>
2021-01-20 16:46:45 +0100

    Chargement

---
+++ Assets/Scripts/EMD_Scripts/EMD_Loading.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EMD_Loading : MonoBehaviour
+{
+    public GameObject LoadingCanvas;
+    private ELC_RoomsGenerator RoomsGeneratorScript;
+
+    private void Start()
+    {
+        RoomsGeneratorScript = FindObjectOfType<ELC_RoomsGenerator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (RoomsGeneratorScript.GenerationFinish == true)
+        {
+            LoadingCanvas.SetActive(false);
+        }
+    }
+}
