beae181023b82fd9945469f5cf2b7b7c6ccc923d
Eliott <eliott.chauchard@gmail.com>
2021-01-20 18:26:39 +0100

    Des +1 fouah le truc de ouf

---
+++ Assets/ELC_CompteRebours.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ELC_CompteRebours : MonoBehaviour
+{
+    public float timeBeforeDestruct;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("compte");
+    }
+
+    IEnumerator compte()
+    {
+        yield return new WaitForSeconds(timeBeforeDestruct);
+        Destroy(this.gameObject);
+    }
+}
