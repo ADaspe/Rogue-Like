7ff8db1c6018fa2abc2ae3ab3831c25f49a49643
Geoff <geoffroy.carrot@rubika-edu.com>
2021-01-20 15:10:49 +0100

    lowlife ajouté sfx

--- Assets/GFC_Footsteps.cs
+++ Assets/GFC_Footsteps.cs
@@ -19,6 +19,7 @@ public class GFC_Footsteps : MonoBehaviour
     public AudioSource orangeSponk;
     public AudioSource redSponk;
     public AudioSource bouteille;
+    public AudioSource lowlife;
     
     bool deathAlreadyPlayed = false;
     public float nextSponkAttackTime;
@@ -95,6 +96,10 @@ public class GFC_Footsteps : MonoBehaviour
         if (Input.GetAxisRaw("Heal") == 0 && bouteille.isPlaying == true)
         {
             bouteille.Stop();
+        }
+        if (!lowlife.isPlaying && statManager.currentHealth <= statManager.MaxHealth * statManager.LifeStopDecrease / 100)
+        {
+            lowlife.Play();
         }
 
         
